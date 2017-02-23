2e4e818a3289f997f541f523606f9235422c7fad
JusteRaimbault <raimbaultjwin@gmail.com>
2017-02-23 17:24:33 +0100

    setup_paper_repo

--- Models/analysis/analysis.R
+++ Models/analysis/analysis.R
@@ -4,7 +4,7 @@ library(ggplot2)
 
 setwd(paste0(Sys.getenv('CS_HOME'),'/MigrationDynamics/Models/exploration'))
 
-res <- as.tbl(read.csv('2016_12_03_02_34_10_lhs.csv'))
+res <- as.tbl(read.csv('2016_12_03_14_46_02_grid.csv'))
 res$id = as.character(cumsum(c(1,diff(res$accDecay)>0)))
 
 
@@ -28,18 +28,18 @@ g+geom_density(aes(x=indivMigrations,fill=id),alpha=0.3)
 
 # summarise
 
-sres = res %>% group_by(id)%>% summarise(
+sres = res %>% group_by(accDecay,costAccessRatio,wealthSigma,moveAversion)%>% summarise(
   deltaU0 = mean(deltaU0),deltaU1=mean(deltaU1),indivMigrations=mean(indivMigrations),
   jobDistance0=mean(jobDistance0),jobDistance1=mean(jobDistance1),migration0=mean(migration0),
-  migration1=mean(migration1),wealth=mean(wealth),wealthGain=mean(wealthGain)
-  #transportationCost=mean(transportationCost),gravityDecay=mean(gravityDecay),
-  #distribSd=mean(distribSd),overlapThreshold=mean(overlapThreshold)
+  migration1=mean(migration1),wealth=mean(wealth),wealthGain=mean(wealthGain)#,
+  #accDecay=mean(accDecay),costAccessRatio=mean(costAccessRatio),
+  #wealthSigma=mean(wealthSigma),moveAversion=mean(moveAversion)
 )
 
 
 indic="wealthGain"
-g = ggplot(sres,aes_string(x="accDecay",y="costAccessRatio",fill=indic))
-g+geom_raster(hjust=0,vjust=0)+facet_grid(wealthSigma~moveAversion,scales = "free")+scale_fill_gradient(low='yellow',high='red')
+g = ggplot(sres,aes_string(x="wealthSigma",y="costAccessRatio",fill=indic))
+g+geom_raster(hjust=0,vjust=0)+facet_grid(moveAversion~accDecay,scales = "free")+scale_fill_gradient(low='yellow',high='red')
 
 
 
